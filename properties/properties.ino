5b6430449fce06f7125eb628367435a0d025863f
paodelonga <venombrime@gmail.com>
2023-02-10 17:43:59 -0300

    :sparkles: feat: propriedades sobre o projeto

---
+++ properties/properties.ino
@@ -0,0 +1,24 @@
+class Properties {
+	public:
+	class Project {
+		public:
+		const char* Author = "Author Name";
+		const char* Version = "0.0.0";
+		const char* Rev = "Segunda-Feira, 1 de Janeiro de 1999 - 00:00 AM";
+		const char* Github = "https://github.com/user/projectName";
+	}
+	Project;
+}
+Properties;
+
+void setup() {
+	Serial.begin(9600);
+
+	Serial.println(Properties.Project.Author);
+	Serial.println(Properties.Project.Version);
+	Serial.println(Properties.Project.Rev);
+	Serial.println(Properties.Project.Github);
+}
+
+void loop() {
+}
